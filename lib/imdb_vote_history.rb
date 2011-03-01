45f4f592ea727b9d7091d075b6e8f24bfa09ab6e
Linus Oleander <linus@oleander.nu>
2011-03-01 03:25:56 +0100

    Reducing the code base by 30% due to the awesome tip by 'Chicago_gangster'. (More info in the readme)

--- lib/imdb_vote_history.rb
+++ lib/imdb_vote_history.rb
@@ -8,11 +8,9 @@ class ImdbVoteHistory
   # Example on valid URL:
   # => http://www.imdb.com/mymovies/list?l=32558051
   def initialize(url)
-    raise ArgumentError.new("The url #{url} is invalid") unless url.to_s.match(/^(http:\/\/)?(w{3}\.)?imdb\.com\/mymovies\/list\?l=\d{2,}$/)
-    @page    = 0
-    @movies  = []
-    @url     = url
-    @content = {}
+    raise ArgumentError.new("The url #{@url} is invalid") unless url.to_s.match(/^(http:\/\/)?(w{3}\.)?imdb\.com\/mymovies\/list\?l=\d{2,}$/)
+    @movies = []
+    @url    = "#{url}&a=1"
   end
   
   # Fetches movies for the given URL.
@@ -41,13 +39,7 @@ class ImdbVoteHistory
   # A unique id for this particular list.
   # Return type is Fixnum.
   def id
-    url.match(/list\?l=(\d+)/).to_a[1].to_i
-  end
-  
-  # Fetches the {value} page.
-  # Take one argument, of type Fixnum.
-  def page(value)
-    @page = value * 10; self
+    @url.match(/list\?l=(\d+)/).to_a[1].to_i
   end
   
   # Should we fetch all movies, even though pagination exists?
@@ -64,45 +56,15 @@ class ImdbVoteHistory
     def prepare!
       movies = []; content.css("td.standard a").each do |movie|
         movie = Container::Movie.new(:imdb_link => movie.attr("href"))
-        movies << movie if movie.valid?
+        @movies << movie if movie.valid?
       end
-        
-      @movies += movies; prepare! if @all and movies.any? and step! and not done?
-    end
-    
-    def done?
-      not content.content.empty? and total_results == current_results
-    end
-
-    def total_results
-      content.at_css(".standard:nth-child(1)").content.match(/: (\d+)/).to_a[1]
-    end
-
-    def results
-      current_results.to_i - current_display[1].to_i + 1
-    end
-
-    def current_results
-      current_display[2]
-    end
-
-    def current_display
-      content.at_css("tr:nth-child(2) .standard b:nth-child(1)").content.match(/(\d+)-(\d+)/)
-    end
-    
-    def url
-      @page.zero? ? @url : "#{@url}&o=#{@page}"
-    end
-    
-    def step!
-      @page += 10
     end
     
     def download
-      RestClient.get(url, :timeout => 10) rescue ""
+      RestClient.get(@url, :timeout => 10) rescue ""
     end
 
     def content
-      @content[@page] ||= Nokogiri::HTML download
+      @content ||= Nokogiri::HTML download
     end
 end
--- spec/imdb_vote_history_spec.rb
+++ spec/imdb_vote_history_spec.rb
@@ -1,13 +1,18 @@
 describe ImdbVoteHistory do
   before(:all) do
-    @url = "http://www.imdb.com/mymovies/list?l=32558051"
-    @count = 937
-    @valid_urls = ["http://imdb.com/mymovies/list?l=123431", "imdb.com/mymovies/list?l=3212312351", "www.imdb.com/mymovies/list?l=31231251"]
+    @url          = "http://www.imdb.com/mymovies/list?l=32558051"
+    @valid_urls   = ["http://imdb.com/mymovies/list?l=123431", "imdb.com/mymovies/list?l=3212312351", "www.imdb.com/mymovies/list?l=31231251"]
+    @invalid_urls = ["http://www.imdb.com/mymovieslist?l=32558051", "mymovieslist?l=32558051", "http://www.imdb.com/mymovieslist?l=32558abc051"]
+    @count        = 937
+  end
+  
+  after(:each) do
+    WebMock.reset!
   end
   
   context "the find_by_url method if success" do
     before(:each) do
-      stub_request(:any, @url).to_return(:body => File.new("spec/fixtures/32558051.html"), :status => 200)
+      stub_request(:get, "#{@url}&a=1").to_return(:body => File.new("spec/fixtures/32558051.html"), :status => 200)
     end
     
     it "should return a list of movies" do
@@ -34,7 +39,7 @@ describe ImdbVoteHistory do
     end
     
     it "should raise an exception if the url is invalid" do
-      ["http://www.imdb.com/mymovieslist?l=32558051", "mymovieslist?l=32558051", "http://www.imdb.com/mymovieslist?l=32558abc051"].each do |url|
+      @invalid_urls.each do |url|
         lambda { ImdbVoteHistory.find_by_url(url) }.should raise_error(ArgumentError)
       end
       
@@ -44,16 +49,16 @@ describe ImdbVoteHistory do
     end
   end
   
-  context "the find_by_url method if 404 error" do
+  context "the find_by_url method if 404 error" do    
     before(:each) do
-      stub_request(:any, @url).to_return(:body => "", :status => 404)
+      stub_request(:get, "#{@url}&a=1").to_return(:body => "", :status => 404)
     end
     
     it "should return an empty list if something goes wrong" do
-      ImdbVoteHistory.find_by_url(@url).should have(0).movies
+      ImdbVoteHistory.find_by_url(@url).movies.should be_empty
     end
     
-    it "should return an empty user" do
+    it "should not return a user" do
       ImdbVoteHistory.find_by_url(@url).user.should be_nil
     end
   end
@@ -67,50 +72,6 @@ describe ImdbVoteHistory do
       ["32558051", 32558051].each do |id|
         lambda { ImdbVoteHistory.find_by_id(id) }.should_not raise_error(ArgumentError)
       end
-    end
-  end
-  
-  context "pagination" do
-    def rest_client(url, file)
-      RestClient.should_receive(:get).with(url, {:timeout=>10}).and_return(File.new("spec/fixtures/#{file}.html"))
-    end
-    
-    before(:each) do
-      @pagination_url = "http://www.imdb.com/mymovies/list?l=19736607"
-      stub_request(:any, @pagination_url).to_return(:body => File.new("spec/fixtures/pagination.html"), :status => 200)
-    end
-    
-    it "should know how many results that are shown" do
-      ImdbVoteHistory.find_by_url(@pagination_url).should have(10).movies
-    end
-    
-    it "should be possible to step" do
-      ivh = ImdbVoteHistory.find_by_url(@pagination_url)
-      10.times { lambda { ivh }.should change(ivh, :step!).by(10) }
-    end
-    
-    it "should be possible to get all results" do
-      rest_client("http://www.imdb.com/mymovies/list?l=19736607", "pagination")                                              # First page
-      rest_client("http://www.imdb.com/mymovies/list?l=19736607&o=560", "pagination_end")                                    # Last page
-      (10..550).step(10).each { |page| rest_client("http://www.imdb.com/mymovies/list?l=19736607&o=#{page}", "pagination") } # Everything in between
-      
-      ImdbVoteHistory.find_by_url(@pagination_url).all.should have(560).movies
-    end
-    
-    it "should not step when all movies is being displayed on the first page" do
-      stub_request(:get, @url).to_return(:body => File.new("spec/fixtures/32558051.html"), :status => 200)
-      
-      ImdbVoteHistory.find_by_url(@url).all.should have(@count).movies
-      a_request(:get, @url).should have_been_made.times(1)
-    end
-  end
-  
-  context "the page method" do
-    it "should be possible to set a page" do
-      stub_request(:get, @url).to_return(:body => File.new("spec/fixtures/32558051.html"), :status => 200)
-      
-      ImdbVoteHistory.find_by_url("http://www.imdb.com/mymovies/list?l=19736607").page(4).movies
-      a_request(:get, "http://www.imdb.com/mymovies/list?l=19736607&o=40").should have_been_made.times(1)
     end
   end
 end
